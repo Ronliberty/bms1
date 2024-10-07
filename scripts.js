5ad23971235f3e2e3a4bac09ca91a0f86ad8d972
Ronliberty <125681174+Ronliberty@users.noreply.github.com>
2024-10-07 13:01:16 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Toggle sidebar visibility for mobile
+document.getElementById('sidebarToggle').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+  });
+  
